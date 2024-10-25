37167f314857e66508dcd79644508773d841c1d7
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2024-10-25 12:05:16 +0300

    Delete Lab2/src/main.clj

--- Lab2/src/main.clj
+++
@@ -1,8 +0,0 @@
-(ns main)
-(defn create-bag [] {})
-(defn insert
-  ([bag element]
-   (if (zero? (count bag))
-     (insert bag element (Math/floor (/ (count bag) 2)) 0 (count bag))
-     (conj bag element)))
-  ([bag element now start end] (if ())))
