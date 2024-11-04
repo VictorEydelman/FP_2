ed324fca22e4b1023d125d0875c9564f590ce8dd
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2024-11-04 14:21:23 +0300

    Update propertyTest.clj

--- Lab2/test/propertyTest.clj
+++ Lab2/test/propertyTest.clj
@@ -20,14 +20,12 @@
       (= (delete (add bag element) element) bag))
     ))
 
-#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defspec merge-empty-bag
   100
   (prop/for-all [node gen_bag]
     (let [bag (if (= node []) (create-bag) (reduce add (create-bag) node))]
       (= (merger bag (create-bag)) bag))
     ))
-#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defspec monoid
   100
   (prop/for-all [node gen_bag]
@@ -35,7 +33,6 @@
       (= (merger (merger bag bag) bag)
          (merger bag (merger bag bag))))
     ))
-#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defspec polymorphic
   100
   (prop/for-all [element (gen/one-of [gen/int gen/char gen/boolean gen/string gen/double
